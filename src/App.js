22d12ea3334d4a9845fbcd91ee11be0d6fcc4fb4
Felipe <87789520+FelipeSilva916@users.noreply.github.com>
2022-06-13 13:27:42 -0700

    set up folders

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Home from "./components/Home";
 
 function App() {
-  return <h1>a/A Forms</h1>;
+  return (
+    <>
+      <h1>a/A Forms</h1>
+      <Home />
+    </>
+  );
 }
 
 export default App;
